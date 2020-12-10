4eaeaf27d2ada31b5a3279ebec739cc72ec20574
Anna Roshchina <annie.roshchina@yahoo.com>
2020-12-10 19:04:13 +0100

    components to choose sounds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Home from './components';
 
 function App() {
 
   return (
     <div className="App">
       <h1>Here will be my App</h1>
+      <Home />
     </div>
   );
 }
